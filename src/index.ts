957b34a6ec7b09463d0922c549c688e078b72144
Nicholas Wehr <github@biochx.com>
2024-11-27 18:01:53 -0600

    wip

--- src/index.ts
+++ src/index.ts
@@ -4,40 +4,22 @@ import chalk from "chalk";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-/*
-import { createSignerFromKey } from "#/payment";
-import type { ProgramId, Uuid } from "#/types";
-import { type ValuesPermissions, ValuesPermissionsBuilder } from "#/types";
-import { type VmClient, VmClientBuilder } from "#/vm";
-
-import { NadaValue } from "@nillion/client-wasm";
-import { NamedNetwork } from "@nillion/client-core";
-import { createSignerFromKey } from "@nillion/client-payments";
-import { NillionClient } from "@nillion/client-vms";
-*/
-
 import { Argv } from "yargs";
 import { promises as fsPromises } from "fs";
 import { config } from "dotenv";
 import { homedir } from "os";
 import { join } from "path";
 
-// Construct the path to the .env file in the home directory
 const envPath = join(homedir(), ".config/nillion/nillion-devnet.env");
-
-// Load the .env file
 config({ path: envPath });
 
-// import type { VmClient } from "@nillion/client-vms";
 import { NadaValue } from "@nillion/client-vms";
+import { createClient } from "@nillion/client-react-hooks";
 
 
 // BROKEN! how do I import this member?
 // import { ValuesPermissionsBuilder } from "@nillion/client-vms/types/values-permissions";
 
-import { createClient } from "@nillion/client-react-hooks";
-
-// Define the CLI commands
 yargs(hideBin(process.argv))
   .scriptName("verida-poster")
   .usage("$0 <command> [options]")
